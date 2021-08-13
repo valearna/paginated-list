6f4c5d2f4d3cb94e2c4626449f578301c871f93b
Valerio Arnaboldi <valearna@caltech.edu>
2021-08-13 10:37:22 -0700

    chore(version): new major version

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,11 @@
-import babel from 'rollup-plugin-babel'
-import commonjs from 'rollup-plugin-commonjs'
-import external from 'rollup-plugin-peer-deps-external'
-import postcss from 'rollup-plugin-postcss'
-import resolve from 'rollup-plugin-node-resolve'
-import url from 'rollup-plugin-url'
-import svgr from '@svgr/rollup'
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import external from 'rollup-plugin-peer-deps-external';
+import postcss from 'rollup-plugin-postcss';
+import url from 'rollup-plugin-url';
+import svgr from '@svgr/rollup';
 
-import pkg from './package.json'
+import pkg from './package.json';
 
 export default {
   input: 'src/index.js',
@@ -31,7 +30,7 @@ export default {
     svgr(),
     babel({
       exclude: 'node_modules/**',
-      plugins: [ 'external-helpers' ]
+      plugins: ['external-helpers']
     }),
     commonjs({
       include: 'node_modules/**',
@@ -40,4 +39,4 @@ export default {
       }
     })
   ]
-}
+};
